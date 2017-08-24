d6635e8954d1d0c94f180a17b55a87fca7fef39d
Shen <xuhui.shen@nokia.com>
2017-08-24 17:31:57 +0800

    ad token model

--- examples/models.py
+++ examples/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
 class Action(models.Model):
     '''交易动作：买入，卖出
     '''
