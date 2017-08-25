be0d06f154881dfdf8f7d0fe66d8bf1d5c5b2e74
Shen <xuhui.shen@nokia.com>
2017-08-25 14:04:55 +0800

    support token

--- lhjy/settings.py
+++ lhjy/settings.py
@@ -110,6 +110,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
