3a6dc07ac49a3b8c82afe314b9fe624712f0cdc8
Shen <xuhui.shen@nokia-sbell.com>
2017-10-09 13:58:40 +0800

    add allow host

--- lhjy/settings.py
+++ lhjy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '20fdhwiy^5065c=)1bpgyw_6sa1=*w8kl4gh+*lwulvt=&62q@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
