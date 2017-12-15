3ee6642153336031fabb8503c2bfd69af13d1cc0
xuhshen <xuhshen@163.com>
2017-12-15 14:16:54 +0800

    revert dhango version

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'', views.index, name='index'),
 ]
