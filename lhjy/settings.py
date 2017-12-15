e4975dc59029311e9747cd6323b56bc4c36b003e
xuhshen <xuhshen@163.com>
2017-12-15 13:40:25 +0800

    udate setting

--- lhjy/settings.py
+++ lhjy/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"assets"),
 ) 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 SOCKET_SERVER = "127.0.0.1"
 SOCKET_PORT = 9090
